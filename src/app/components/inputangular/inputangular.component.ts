8ce9d9e3694bd4d33058eed98c101ff7006ab8a1
dungnt2khust <dunggamo2000@gmail.com>
2022-05-18 10:18:04 +0700

    add input

---
+++ src/app/components/inputangular/inputangular.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'inputangular',
+  templateUrl: './inputangular.component.html',
+  styleUrls: ['./inputangular.component.scss'],
+})
+export class InputangularComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
