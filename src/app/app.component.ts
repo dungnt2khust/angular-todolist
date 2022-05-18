21bbe3e84c5712fa20fdeeffd40b9e97297e9095
dungnt2khust <dunggamo2000@gmail.com>
2022-05-18 17:22:06 +0700

    fix hover mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,8 @@ export class AppComponent implements OnInit, OnChanges, OnDestroy {
   constructor(private meta: Meta) {
     this.meta.addTag({
       name: 'viewport',
-      content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+      content:
+        'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
     });
   }
 
