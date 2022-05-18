7e8d85da199d501b2b25c7f967b7aca1deabdbf1
dungnt2khust <dunggamo2000@gmail.com>
2022-05-18 10:40:34 +0700

    add lib scss

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,9 +2,9 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `<h5 class="position-fixed">{{name}}</h5>`,
+  styles: [`h5 { font-family: sans-serif; bottom: 10px; right: 10px; }`],
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
 }
